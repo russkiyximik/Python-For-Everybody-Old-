a29c9abd4f575cc9a986feb454a8429ee67d1022
russkiyximik <alex.ponomar37@gmail.com>
2025-06-09 23:25:24 +0000

    all done

--- Intro Python Coursera/Testing/main.py
+++ Intro Python Coursera/Testing/main.py
@@ -8,3 +8,27 @@ for i in range(3):
     val = multiplier(val)
 
 print(val)
+
+# Trying a new thing
+
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    try:
+        n = int(num)
+    except:
+        if num == "done":
+            break
+        else:
+            print("Invalid input")
+    if largest is None and smallest is None:
+            largest = n
+            smallest = n
+    elif n > largest:
+            largest = n
+    elif n < smallest:
+            smallest = n
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
